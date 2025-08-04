6d855c8dc3097a4d157f787cbaf89375c85cd7ff
seunggonhyeon <hyunsg5638@gmail.com>
2025-08-04 23:48:14 +0900

    design:first component

---
+++ components/MyComponents.tsx
@@ -0,0 +1,15 @@
+const React = require("react");
+const ReactNative = require("react-native");
+const { View, Text } = ReactNative;
+
+const MyComponents = React.createReactClass({
+  render() {
+    return (
+      <View>
+        <Text>My Components</Text>
+      </View>
+    );
+  },
+});
+
+export default MyComponents;
